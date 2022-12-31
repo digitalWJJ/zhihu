b71ce3871e82553a4d90f8470a0707fd39ab3209
digitalWJJ <103307041+digitalWJJ@users.noreply.github.com>
2022-12-31 10:55:32 +0800

    upload index.js
    第一次
    上传index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+import MainStack from './routes/index'
+
+export default class Index extends Component {
+  render() {
+    return (
+      <MainStack />
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
